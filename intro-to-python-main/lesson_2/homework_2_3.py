e64244ed83eb6be6d184097f9526e24e995c4399
Ella Ellovna <ella.ellovna@gmail.com>
2022-01-11 12:43:16 -0500

    Homework 3 commit

--- intro-to-python-main/lesson_2/homework_2_3.py
+++ intro-to-python-main/lesson_2/homework_2_3.py
@@ -3,6 +3,7 @@
 # !important you should save only number to result_temperature. Formula (32°F − 32) × 5/9 = 0°C
 
 # type your code here
-temperature_fahrenheit = int("75")
-result_temperature = (temperature_fahrenheit - 32) * 5/9
-print(int(result_temperature))
+temperature_fahrenheit = int(input("Please enter temperature in Farenheit in your city: "))
+result_temperature = round((temperature_fahrenheit - 32) * 5/9, 2)
+# result_temperature = round(result_temperature, 2)
+# print("The temperature in your city is: " + str(result_temperature))
